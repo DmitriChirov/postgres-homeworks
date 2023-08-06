a1794813d47b7d3edfece94d52d5b570671a20e7
DmitriChirov <dim.ch.it80@gmail.com>
2023-08-06 21:58:50 +0400

    defined the function for reading csv-file

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,15 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+def csv_reader(path):
+    """Читает CSV-файл и возвращает объект со списками значений.
+    Функция предполагает наличие header в первой строке CSV-файла и пропускает её"""
+    with open(path, 'r', encoding='utf-8', newline='') as csvfile:
+        next(csvfile)
+        reader = csv.reader(csvfile)
+        return reader
+
+
+# csv_reader('./north_data/customers_data.csv')
